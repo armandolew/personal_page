fd2fd1988b4f85a66bec42e9060274c6d27b815f
Armando Lew <alew16@hotmail.com>
2018-07-23 14:28:41 -0500

    Adding image converter to base 64 on image new form

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,16 @@
 class Image < ApplicationRecord
   belongs_to :imageable, polymorphic: true, optional: true
+
+  attr_accessor :image_path
+
+  before_create do
+    p self.inspect
+    unless self.image_path
+      errors.add :base, 'image path not present'
+    end
+
+    image_converted_string = Base64.encode64(open(self.image_path).read).gsub("\n", '')
+
+    self.image = image_converted_string
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,5 @@ Rails.application.routes.draw do
   resources :albums
   resources :posts
   resources :users
-
-  namespace 'utils' do
-    get 'image_converter_page'
-    post 'convert_image_to_base'
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
